530cca7bccee69fbe9b49855777274e2527e2f84
José Ibáñez <jose.ibanez@techsbcn.com>
2022-03-24 13:02:20 +0100

    Contributions base

---
+++ src/Contributions/TimelogSummary/TimelogSummary.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import * as SDK from "azure-devops-extension-sdk";
+
+export const TimelogSummary: React.FC = () => {
+    useEffect(() => {
+        SDK.init().then(async () => {
+            SDK.register(SDK.getContributionId(), () => {})
+        })
+    }, [])
+
+    return (
+        <div>TimelogSummary</div>
+    );
+}
