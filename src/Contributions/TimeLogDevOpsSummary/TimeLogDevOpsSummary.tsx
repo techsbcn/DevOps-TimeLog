7c69093a126f21f1d259e63bd34d4b8d6be834c6
Ferran-Castillo <ferran.castillo@techsbcn.com>
2022-04-22 14:49:05 +0200

    remove old files

--- src/Contributions/TimeLogDevOpsSummary/TimeLogDevOpsSummary.tsx
+++ src/Contributions/TimeLogDevOpsSummary/TimeLogDevOpsSummary.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as SDK from 'azure-devops-extension-sdk';
 import { showRootComponent } from '../..';
 import { useFetchGetDocumentsWithoutFiltersQuery } from '../../redux/extensionDataManager/extensionDataManagerSlice';
-import { TimeLogSummary } from '../../components/timeLogSummary/TimeLogSummary';
+import TimeLogSummary from '../../components/timeLogSummary/TimeLogSummary';
 import { CircularProgress, Box } from '@mui/material';
 
 export const TimeLogDevOpsSummary: React.FC = () => {
