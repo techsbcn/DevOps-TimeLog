c08ac7857b1ddf3135d27dc2f958b2564cadebd8
Ferran Castillo Beltri <ferran.castillo@techsbcn.com>
2022-07-15 13:00:07 +0200

    add new scope

--- devops-timelog.config.js
+++ devops-timelog.config.js
@@ -89,7 +89,7 @@ module.exports = (env) => {
         },
       },
     ],
-    scopes: ['vso.project_manage', 'vso.code', 'vso.graph', 'vso.work'],
+    scopes: ['vso.project_manage', 'vso.code', 'vso.graph', 'vso.work', 'vso.analytics'],
     screenshots: [
       {
         path: 'images/time-log-new-entry.png',
